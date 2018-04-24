d7653409194103badf16b17b61e2b3f93617bbea
mmoharem <cloud@bnfstore.com>
2018-04-24 17:16:21 +0200

    finish relation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,12 @@
                             <tr>
                                 <td>{{$post->title}}</th>
                                 <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
-                                <td></td>
+                                <td>
+                                    {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                        {{Form::hidden('_method', 'DELETE')}}
+                                        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                    {!!Form::close()!!}
+                                </td>
                             </tr>
                         @endforeach
                     </table>
